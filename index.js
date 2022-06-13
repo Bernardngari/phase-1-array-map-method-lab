0b91cf8a813f35dfa91418a948c59e7eee763727
Bernardngari <bernardngari.ke@gmail.com>
2022-06-13 14:50:18 +0300

    commit tree 3e5cff6f86e0d67fd11a5958c5e7c4dee29cd258

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// const tutorials = [
+//   'what does the this keyword mean?',
+//   'What is the Constructor OO pattern?',
+//   'implementing Blockchain Web API',
+//   'The Test Driven Development Workflow',
+//   'What is NaN and how Can we Check for it',
+//   'What is the difference between stopPropagation and preventDefault?',
+//   'Immutable State and Pure Functions',
+//   'what is the difference between == and ===?',
+//   'what is the difference between event capturing and bubbling?',
+//   'what is JSONP?'
+// ];
+
+// const titleCased = () => {
+//   return tutorials
+// }
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -12,5 +29,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => {
+    return (title.split(' ').map(word => {
+      return word[0].toUpperCase() + word.substring(1); 
+    })).join(' ');
+  })
 }
